15680ed3a192b846df021ef8d513428880ec48f2
Oral <kdirbaevoral875@gmial.com>
2024-03-22 20:19:23 +0300

    1.0.2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ function App() {
   return (
     <div className="App">
       <h1>Hello Reactjs!</h1>
+      <h1>Hello Reactjs!</h1>
+      <h1>Hello Reactjs!</h1>
+      <h1>Hello Reactjs!</h1>
+      <h1>Hello Reactjs!</h1>
     </div>
   );
 }
